6862f6d9bb069be54c78ae9913a419eae28af095
Lloyd <stevens.lloyd@Gmail.com>
2023-02-26 20:49:52 +0000

    bootstrap nav

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 from judgements import *
 app = Flask(__name__)
 
-
+user = 'Joe Bloggs'
 
 @app.route("/")
 def hello_world():
   return render_template('home.html', 
                          company='PLC', 
-                        options =  OPTIONS)
+                        options =  OPTIONS, 
+                        user = 'Joe Bloggs')
 
 
 # this will run the app when run is pressed
